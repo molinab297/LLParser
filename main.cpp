b70f7794986e7df7c274ab7e2c45a6d837f315f7
mcorrente1 <mattcorrente@Matts-MBP.attlocal.net>
2017-04-29 23:26:47 -0700

    fixed bugs caused by faulty merge

--- main.cpp
+++ main.cpp
@@ -8,15 +8,11 @@ int main() {
     const string NONTERMINALS_FILE = "resources/nonterminals.txt";
     const string INPUT_FILE = "resources/finalv1.txt"; //original input file
     const string CLEAN_FILE = "resources/finalv2.txt"; //cleaned input file ready for parsing
-    const string INPUT_FILE = "finalv1.txt"; //original input file
-    const string CLEAN_FILE = "finalv2.txt"; //cleaned input file ready for parsing
     const regex REGULAR_EXPRESSION("END[.]|\/\/[^\/]*\/\/|-?[0-9]+|[a-zA-Z]+[0-9]*[a-zA-Z]*|[*/+-=,()]"); //regular expression that cleans the input file according to rules of established language
-
 
     //output clean version of original input file to new file
     cleanFile(INPUT_FILE, CLEAN_FILE, REGULAR_EXPRESSION);
-
-
+    
     PredictiveParser parser(NUM_ROWS, NUM_COLS, TERMINALS_FILE, NONTERMINALS_FILE, PARSING_TABLE_FILE);
 
     if(parser.validateCode(CLEAN_FILE))
