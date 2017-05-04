1d9abf6360282a834b6cfa23334233a3356ba54a
amadodabeast <amadodabeast@csu.fullerton.edu>
2017-05-03 19:27:30 -0700

    Add files via upload
    part 3 output test by testrun.txt

---
+++ output.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int P1 , Q2S , R , S12 ; 
+P1 = - 3 ; 
+Q2S = 4 ; 
+R = 5 ; 
+cout << R << endl;
+endl;
+S12 = P1 * ( Q2S + 2 * R ) ; 
+cout << S12 << endl;
+endl;
+return 0;
+}
