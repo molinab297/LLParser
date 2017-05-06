43267ddd79edaed435008390f873171e80b852f3
mattcorrente <mcorrente72@gmail.com>
2017-05-05 21:31:23 -0700

    program now accepts any program name that falls within accepted grammar, program now compiles, runs, and displays output of final cpp file, translateToCPP function needs to be completed still

--- myHeader.h
+++ myHeader.h
@@ -5,10 +5,14 @@
 #include <string>
 #include "cleanFile.h"
 #include "PredictiveParser.h"
+#include "translateToCPP.h"
 using namespace std;
 
 //Purpose: Accepts an input file then uses the passed in regular expression to iterate through regex matches and
 //output those matches to the passed in output file
 void cleanFile(string inputFile, string outputFile, regex regexExpression);
+
+//Purpose: Accepts an input file then rewrites it to passed in outfile converted to C++
+void translateToCPP(string inFileName, string outFileName);
 
 #endif //COMPILER_MYHEADER_H
---
+++ translateToCPP.h
@@ -0,0 +1,89 @@
+/*
+ * translateToCPP.h
+ *
+ * Purpose: Accepts an input file then rewrites it to passed in outfile converted to C++
+ *
+ */
+
+#ifndef FINAL_PROJECT_COMPILERS_TRANSLATETOCPP_H
+#define FINAL_PROJECT_COMPILERS_TRANSLATETOCPP_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <regex>
+#include <iomanip>
+
+using namespace std;
+
+void translateToCPP(string inFileName, string outFileName){
+    ofstream fout(outFileName);
+    ifstream fin(inFileName);
+    string temp;
+    string fileString;
+    regex expression("[A-Za-z]+[0-9]*[A-Za-z]*|-?[0-9]+;|[*/+-=,()]");
+    while (!fin.eof())
+    {
+        getline(fin, temp);
+        fileString.append(temp + '\n');
+    }
+    fin.close();
+    regex_iterator<string::iterator> rit(fileString.begin(), fileString.end(), expression);
+    regex_iterator<string::iterator> rend;
+
+    while (rit != rend)
+    {
+        if (rit->str() == "PROGRAM")
+        {
+            fout << "#include <iostream>\nusing namespace std;\nint main()\n{";
+            while (rit->str() != ";")
+                rit++;
+            if (rit->str() == ";")
+            {
+                fout << endl;
+            }
+
+        }
+        else if (rit->str() == "END")
+        {
+            fout << "return 0;\n}" << endl;
+        }
+        else if (rit->str() == "INTEGER")
+        {
+            fout << "    int ";
+        }
+        else if (rit->str() == ";")
+        {
+
+            fout << "; " << endl;
+        }
+        else if (rit->str() == "PRINT")
+        {
+            fout << "cout << ";
+            rit++;
+            while (rit->str() != ";")
+            {
+                if (rit->str() != "(" && rit->str() != ")")
+                {
+                    fout << rit->str() << " << ";
+                }
+                rit++;
+            }
+            if (rit->str() == ";")
+            {
+                fout << "endl;" << endl;
+                fout << "endl;" << endl;
+            }
+        }
+        else if (rit->str() != ":" && rit->str() != "BEGIN")
+        {
+            fout << rit->str() << " ";
+        }
+        rit++;
+
+    }
+
+}
+
+
+#endif //FINAL_PROJECT_COMPILERS_TRANSLATETOCPP_H
