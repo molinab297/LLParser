979137b7342152459396c08cbbec0d5202500333
molinab297 <molinab297@gmail.com>
2017-05-06 12:18:23 -0700

    closes part 3

--- translateToCPP.h
+++ translateToCPP.h
@@ -11,34 +11,81 @@ using namespace std;
 
 void translateToCPP(string inFileName, string outFileName){
 
-    cout << "Translating to C++ ... ";
-    ifstream inFile(inFileName);
-    ofstream outFile(outFileName);
-    outFile << "#include <iostream>" << "\nusing namespace std;" << "\nint main()" << "\n{\n";
-    string str;
-    getline(inFile, str);
-
-    while(!inFile.eof()){
-        inFile >> str;
-        if(str == "INTEGER")
-            outFile << "int ";
-        else if(str == "PRINT") {
-            outFile << "cout<<";
-            inFile >> str;
-            inFile >> str;
-            outFile << str << "<<endl ";
-            inFile >> str;
-        }
-        else if(str == ";")
-            outFile << ";\n";
-        else if(str == "BEGIN" || str == "END." || str == ":")
-            continue;
-        else {
-            outFile << str + " ";
-
-        }
+    ofstream fout(inFileName);
+    ifstream fin(outFileName);
+    string temp;
+    string fileString;
+    regex expression("[A-Za-z]+[0-9]*[A-Za-z]*|-?[0-9]+;|[*/+-=,()]");
+    while (!fin.eof())
+    {
+        getline(fin, temp);
+        fileString.append(temp + '\n');
     }
-    outFile << "\nreturn 0;" << "\n}";
+    fin.close();
+    regex_iterator<string::iterator> rit(fileString.begin(), fileString.end(), expression);
+    regex_iterator<string::iterator> rend;
+
+    while (rit != rend)
+    {
+        if (rit->str() == "PROGRAM")
+        {
+            cout << "#include <iostream>\nusing namespace std;\nint main()\n{";
+            fout << "#include <iostream>\nusing namespace std;\nint main()\n{";
+            while (rit->str() != ";")
+                rit++;
+            if (rit->str() == ";")
+            {
+                cout << endl;
+                fout << endl;
+            }
+
+        }
+        else if (rit->str() == "END")
+        {
+            cout << "return 0;\n}" << endl;
+            fout << "return 0;\n}" << endl;
+        }
+        else if (rit->str() == "INTEGER")
+        {
+            cout << "    int ";
+            fout << "    int ";
+        }
+        else if (rit->str() == ";")
+        {
+            cout << "; " << endl;
+            cout << "    ";
+            fout << "; " << endl;
+        }
+        else if (rit->str() == "PRINT")
+        {
+            cout << "cout << ";
+            fout << "cout << ";
+            rit++;
+            while (rit->str() != ";")
+            {
+                if (rit->str() != "(" && rit->str() != ")")
+                {
+                    cout << rit->str() << " << ";
+                    fout << rit->str() << " << ";
+                }
+                rit++;
+            }
+            if (rit->str() == ";")
+            {
+                cout << "endl;" << endl;
+                cout << "    ";
+                fout << "endl;" << endl;
+                fout << "endl;" << endl;
+            }
+        }
+        else if (rit->str() != ":" && rit->str() != "BEGIN")
+        {
+            cout << rit->str() << " ";
+            fout << rit->str() << " ";
+        }
+        rit++;
+
+    }
 }
 
 
