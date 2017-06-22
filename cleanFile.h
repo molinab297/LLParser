96c815e236fa55e54980ed83e5b02d8d45ab9489
Blake Molina <molinab297@gmail.com>
2017-06-21 20:29:08 -0700

    bug fixes

--- cleanFile.h
+++ cleanFile.h
@@ -1,7 +1,7 @@
 /**
     cleanFile.h
 
-     Accepts an input file then uses the passed in regular expression to iterate through regex matches and
+    Accepts an input file then uses the passed in regular expression to iterate through regex matches and
     output those matches to the passed in output file
 */
 
